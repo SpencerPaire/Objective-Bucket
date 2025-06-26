2b7a872e6fbd9ed81275521050da190f75907121
Paire Tech <112361841+SpencerPaire@users.noreply.github.com>
2025-06-26 00:05:11 -0400

    Added Integration Test to demo important stuff.

---
+++ ObjectiveBucket/Button.h
@@ -0,0 +1,41 @@
+#ifndef Button_h
+#define Button_h
+
+#include "Arduino.h"
+#include "Timers.h"
+
+enum class ButtonState {
+  Press = 0,
+  Release = 1,
+  Hold = 2,
+};
+
+struct ButtonData {
+  ButtonState event;
+  union
+  {
+    Ticks_t holdTime;
+    Ticks_t releasedTime;
+  };
+};
+
+typedef void (*ButtonCallback)(void *context, ButtonData data);
+
+class Button {
+private:
+  int pin;
+  int heldPeriods;
+  Ticks_t holdTime;
+  ButtonCallback callback;
+  void *context;
+  bool prevState;
+  bool inverted;
+  ButtonState state;
+
+public:
+  Button(int pin, Timers *timers, ButtonCallback callback = NULL, void* context = NULL, Ticks_t holdTime = 1000, bool inverted = true);
+  void Poll(void);
+  ButtonData State(void);
+};
+
+#endif
---
+++ ObjectiveBucket/ObjectiveBucket.ino
@@ -0,0 +1,63 @@
+#include "Timers.h"
+#include "Button.h"
+
+static Timers timers;
+Timer_t *blink;
+
+#define BUTTON0_PIN D3
+#define BUTTON1_PIN D4
+#define BUTTON2_PIN D5
+#define BUTTON3_PIN D6
+#define BUTTON4_PIN D7
+static int ledBlink = LED_BUILTIN ;
+
+
+void ToggleLED(void *context)
+{
+    int led = *(int*)context;
+    digitalWrite(led, !digitalRead(led));
+    //Serial.println("Heartbeat");
+}
+
+void ButtonEvent(void *context, ButtonData data)
+{
+    Timer_t *blink = (Timer_t *)context;
+    if(data.event == ButtonState::Press)
+    {
+        Serial.printf("Button pressed after %d ms\n", data.releasedTime);
+        timers.Pause(blink);
+    }
+    else if(data.event == ButtonState::Release)
+    {
+        Serial.printf("Button released after %d ms\n", data.holdTime);
+        timers.Resume(blink);
+    }
+    else if(data.event == ButtonState::Hold)
+    {
+        Serial.printf("Button held for %d ms\n", data.holdTime);
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN , OUTPUT);
+    digitalWrite(LED_BUILTIN , true);
+    Serial.println("Power On");
+
+    blink = timers.Start(500, ToggleLED, &ledBlink, TimerType::Periodic);
+
+    //static Button buttonReset = Button(BUTTON0_PIN, ButtonEvent, blink, &timers);
+    //static Button buttonReset = Button(BUTTON1_PIN, ButtonEvent, blink, &timers);
+    //static Button buttonReset = Button(BUTTON2_PIN, ButtonEvent, blink, &timers);
+    //static Button buttonReset = Button(BUTTON3_PIN, ButtonEvent, blink, &timers);
+    static Button buttonReset = Button(BUTTON4_PIN, &timers, ButtonEvent, blink);
+
+    // Test code, non-functional
+    // auto lambda = [](void) {ToggleLED();};
+    // timers.Start(500, lambda, TimerType::Periodic );
+}
+void loop()
+{
+    timers.Run();
+}
