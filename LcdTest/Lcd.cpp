88517b4c095d0495c29007f3e2f3d4a915a22c58
Akilan Narayanaswamy <akilan98@gmail.com>
2025-06-26 01:43:20 -0400

    update lcd test to use library

---
+++ LcdTest/Lcd.cpp
@@ -0,0 +1,56 @@
+#include "Lcd.h"
+
+Lcd::Lcd(uint8_t numberOfRows, uint8_t numberOfCols)
+   : numberOfRows(numberOfRows), numberOfCols(numberOfCols)
+{
+   buffer = new char *[numberOfRows];
+   for(int i = 0; i < numberOfCols; i++)
+   {
+      buffer[i] = new char[numberOfCols + 1];
+   }
+   blank = new char[numberOfCols + 1];
+
+   for(int i = 0; i < numberOfCols; i++)
+   {
+      blank[i] = ' ';
+   }
+   blank[numberOfCols] = 0;
+
+   for(int i = 0; i < numberOfRows; i++)
+   {
+      strncpy(buffer[i], blank, numberOfCols + 1);
+   }
+}
+
+void Lcd::Init()
+{
+   lcd.begin(numberOfCols, numberOfRows);
+   lcd.backlight();
+}
+
+void Lcd::WriteMessage(String message, uint8_t row, uint8_t col)
+{
+   if(message.length() < numberOfCols)
+   {
+      for(int i = message.length(); i < numberOfCols; i++)
+      {
+         message.concat(' ');
+      }
+   }
+   strncpy(buffer[row], blank, numberOfCols + 1);
+   strncpy(&buffer[row][col], message.c_str(), message.length());
+}
+
+void Lcd::Update()
+{
+   for(int i = 0; i < numberOfRows; i++)
+   {
+      lcd.setCursor(0, i);
+      lcd.print(buffer[i]);
+   }
+}
+
+Lcd::~Lcd()
+{
+   delete buffer;
+}
