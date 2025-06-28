d0e6661121b7ac494e053c52d93afa2fd3128f15
Paire Tech <112361841+SpencerPaire@users.noreply.github.com>
2025-06-27 22:52:12 -0400

    Fixed some button stuff, added Lifecounter

---
+++ ButtonExample/LifeCounter.cpp
@@ -0,0 +1,108 @@
+#include "LifeCounter.h"
+
+static void GameOver(void *context)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->Pause();
+}
+
+static void UpdateEvent(void *context)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->UpdateScreen();
+}
+
+static void Button0Event(void* context, ButtonData data)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->ButtonEvent(ButtonEnum::Button0, data);
+}
+
+static void Button1Event(void* context, ButtonData data)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->ButtonEvent(ButtonEnum::Button1, data);
+}
+
+static void Button2Event(void* context, ButtonData data)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->ButtonEvent(ButtonEnum::Button2, data);
+}
+
+static void Button3Event(void* context, ButtonData data)
+{
+  GameMode *instance = (GameMode*)context;
+  instance->ButtonEvent(ButtonEnum::Button3, data);
+}
+
+void LifeCounter::ButtonEvent(int button, ButtonData data)
+{
+  if(data.event == ButtonState::Press)
+  {
+    counts[button]++; 
+  }
+}
+
+void LifeCounter::UpdateScreen()
+{
+  this->lcd->ClearScreen();
+
+  String msg = String(counts[0]);
+  this->lcd->WriteMessage(msg, 0, Alignment::Left, false);
+
+  msg = String(counts[1]);
+  this->lcd->WriteMessage(msg, 3, Alignment::Left, false);
+
+  msg = String(counts[2]);
+  this->lcd->WriteMessage(msg, 3, Alignment::Right, false);
+
+  msg = String(counts[3]);
+  this->lcd->WriteMessage(msg, 0, Alignment::Right, false);
+}
+
+LifeCounter::LifeCounter()
+{
+  this->name = "Counters";
+}
+
+void LifeCounter::Init(Timers *timers, Button *buttons[ButtonEnum_Max], Lcd *lcd)
+{
+  Serial.println("Life Counters Selected");
+  this->timers = timers;
+  this->buttons = buttons;
+  this->lcd = lcd;
+
+  this->buttons[ButtonEnum::Button0]->Subscribe(Button0Event, this);
+  this->buttons[ButtonEnum::Button1]->Subscribe(Button1Event, this);
+  this->buttons[ButtonEnum::Button2]->Subscribe(Button2Event, this);
+  this->buttons[ButtonEnum::Button3]->Subscribe(Button3Event, this);
+
+  this->gameTimers[LifeCounter_UpdateTimer] = this->timers->Start(100, UpdateEvent, this,  TimerType::Periodic);
+}
+
+void LifeCounter::Pause()
+{
+
+}
+
+void LifeCounter::Resume()
+{
+
+}
+
+void LifeCounter::DeInit()
+{
+  for(int i = 0; i < LifeCounter_MaxTimers; i++)
+  {
+    this->timers->Stop(this->gameTimers[i]);
+  }
+}
+
+void LifeCounter::Reset()
+{
+  for(int i = 0; i < LifeCounter_TEAMS; i++)
+  {
+    counts[1] = 0;
+  }
+}
